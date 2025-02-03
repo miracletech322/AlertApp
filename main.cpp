515f7d86b64d149d66aae2d642affc2282f2a8df
miracletech322 <miracle.tech322@gmail.com>
2025-02-02 17:50:56 -0800

    QSharedMemory

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 #include <QApplication>
 #include <QMessageBox>
+#include <QSharedMemory>
+
+QSharedMemory shared("AlertApp");
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+    if(!shared.create(512)) {
+        QMessageBox::warning(nullptr, "Instance Already Running", "Another instance of this application is already running.");
+        return -1;
+    }
     MainWindow::getInstance()->show();
     QIcon icon(":/Resource/assets/logo.png");
     MainWindow::getInstance()->setWindowIcon(icon);
