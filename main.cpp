326e5cd103e1730e3965bcfe426c5d8692bfd476
miracletech322 <miracle.tech322@gmail.com>
2025-02-02 17:37:18 -0800

    Original Status

--- main.cpp
+++ main.cpp
@@ -2,19 +2,10 @@
 
 #include <QApplication>
 #include <QMessageBox>
-#include <QProcess>
-#include <QByteArray>
-#include <QStringList>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QProcess process;
-    process.start("taskkill", QStringList() << "/f" << "/im" << "GPShield.exe");
-    process.waitForFinished();
-    QByteArray errorOutput = process.readAllStandardError();
-    qDebug() << errorOutput;
 
     MainWindow::getInstance()->show();
     QIcon icon(":/Resource/assets/logo.png");
